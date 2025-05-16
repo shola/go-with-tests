48d5943c7994b2df5d585847a1c1385758abdb4d
Michael Olusola Situ <michael.o.situ@gmail.com>
2025-05-16 09:11:36 -0700

    refactor: set language prefix in switch statement feat: add french support

--- hello.go
+++ hello.go
@@ -3,18 +3,28 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name, language string) string {
+	var prefix string
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
 	if name == "" {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	return fmt.Sprintf("%s%s", englishHelloPrefix, name)
+	return fmt.Sprintf("%s%s", prefix, name)
 }
 
 func main() {
