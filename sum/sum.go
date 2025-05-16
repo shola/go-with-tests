273a5311c4b4b020e436133185b594dd7da989fb
Michael Olusola Situ <michael.o.situ@gmail.com>
2025-05-16 13:18:49 -0700

    fix: make SumAllTails2 handle empty tail slice test: add tests for empty tail slice refactor: break out assertion logic into helper functions refactor: remove slow/unused function & tests

--- sum/sum.go
+++ sum/sum.go
@@ -20,30 +20,16 @@ func SumAll(numbersToSum ...[]int) []int {
 	return sums
 }
 
-// goos: linux
-// goarch: amd64
-// pkg: notmain
-// cpu: Intel(R) Core(TM) i7-10700 CPU @ 2.90GHz
-// BenchmarkSumAllTails1-16         7053066               187.8 ns/op           144 B/op          6 allocs/op
-// BenchmarkSumAllTails2-16        28372729                42.70 ns/op           24 B/op          2 allocs/op
-// PASS
-// ok      notmain 2.755s
-
-func SumAllTails1(numbersToSum ...[]int) []int {
-	var tailSlices [][]int
-
-	for _, numbers := range numbersToSum {
-		tailSlices = append(tailSlices, numbers[1:])
-	}
-
-	return SumAll(tailSlices...)
-}
-
-func SumAllTails2(numbersToSum ...[]int) []int {
+func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbersToSum {
-		tail := numbers[1:]
+		var tail []int
+		if len(numbers) == 0 {
+			tail = []int{}
+		} else {
+			tail = numbers[1:]
+		}
 		sums = append(sums, Sum(tail))
 	}
 
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -6,53 +6,53 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-
-		got := Sum(numbers)
-		want := 6
-
+	assertNumbersEqual := func(t testing.TB, got, want int, numbers []int) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		assertNumbersEqual(t, got, want, numbers)
+	})
+	t.Run("empty collection", func(t *testing.T) {
+		numbers := []int{}
+		got := Sum(numbers)
+		want := 0
+		assertNumbersEqual(t, got, want, numbers)
 	})
 }
 
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
+	assertSlicesEqual(t, got, want)
+}
 
+func TestSumAllTails(t *testing.T) {
+	t.Run("all slices are non-empty", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 2, 9})
+		want := []int{5, 11}
+		assertSlicesEqual(t, got, want)
+	})
+
+	t.Run("empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{})
+		want := []int{5, 0}
+		assertSlicesEqual(t, got, want)
+	})
+}
+
+func assertSlicesEqual(t testing.TB, got, want []int) {
 	// NOTE: for 2D slices, use reflect.DeepEqual to compare
 	if !slices.Equal(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
-
-func TestSumAllTails1(t *testing.T) {
-	got := SumAllTails1([]int{1, 2, 3}, []int{0, 2, 9})
-	want := []int{5, 11}
-
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-func TestSumAllTails2(t *testing.T) {
-	got := SumAllTails2([]int{1, 2, 3}, []int{0, 2, 9})
-	want := []int{5, 11}
-
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func BenchmarkSumAllTails1(b *testing.B) {
+func BenchmarkSumAllTails(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		SumAllTails1([]int{1, 2, 3}, []int{0, 2, 9})
-	}
-}
-
-func BenchmarkSumAllTails2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		SumAllTails2([]int{1, 2, 3}, []int{0, 2, 9})
+		SumAllTails([]int{1, 2, 3}, []int{0, 2, 9})
 	}
 }
