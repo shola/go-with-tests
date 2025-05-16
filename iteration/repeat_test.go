af9b45bab9f42cdbc4b02a39b6d3c776522dd0a6
Michael Olusola Situ <michael.o.situ@gmail.com>
2025-05-16 10:05:56 -0700

    test: add failing test for arbitrary number fo repeats

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -6,12 +6,22 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat char default number of times", func(t *testing.T) {
+		repeated := Repeat("a")
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("repeat char 'x' number of times", func(t *testing.T) {
+		repeated := Repeat("b", 3)
+		expected := "aaa"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 }
 
 func ExampleRepeat() {
