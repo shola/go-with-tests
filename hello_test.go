401fc80b500d5a0cebc870bb75061d48bd5eaa11
Michael Olusola Situ <michael.o.situ@gmail.com>
2025-05-16 09:00:03 -0700

    refactor: add default name refactor: add subtests refactor: extract assert helper

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Olu")
-	want := "Hello, Olu"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
