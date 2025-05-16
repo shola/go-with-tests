d9dbfd8b8a5fdedb5d9344c4a07b746d3548fe4f
Michael Olusola Situ <michael.o.situ@gmail.com>
2025-05-16 09:06:20 -0700

    refactor: add spanish output option, with test

--- hello.go
+++ hello.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return fmt.Sprintf("%s%s", englishHelloPrefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("Olusola"))
+	fmt.Println(Hello("Olusola", ""))
 }
