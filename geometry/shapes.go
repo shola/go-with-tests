459e6ee1319761d5d9a240ec1a2768574d5f3244
shola <michael.o.situ@gmail.com>
2025-05-18 19:47:19 -0700

    feat: add Rectangle struct, with Perimeter and Area methods feat: add Circle struct, with Perimeter method

--- geometry/shapes.go
+++ geometry/shapes.go
@@ -1,5 +1,24 @@
 package geometry
 
-func Perimeter(height float64, width float64) float64 {
-	return 2 * (height + width);
+import "math"
+
+type Rectangle struct {
+	Height float64
+	Width float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * c.Radius * math.Pi
 }
--- geometry/shapes_test.go
+++ geometry/shapes_test.go
@@ -1,12 +1,36 @@
 package geometry
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
+func TestRectangle(t *testing.T) {
+	r := Rectangle{Height: 10.0, Width: 10.0}
+	t.Run("Perimeter", func (t *testing.T) {
+		got := r.Perimeter()
+		want := 40.0
+		assertFloat(t, got, want)
+	})
+	t.Run("Area", func(t *testing.T) {
+		got := r.Area()
+		want := 100.0
+		assertFloat(t, got, want)
+	})
+}
 
+func TestCircle(t *testing.T) {
+	c := Circle{Radius: 10.0}
+	t.Run("Perimeter", func (t *testing.T) {
+		got := c.Perimeter()
+		want := 2 * 10.0 * math.Pi
+		assertFloat(t, got, want)
+	})
+}
+
+func assertFloat(t testing.TB, got, want float64) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+		t.Errorf("got %g want %g", got, want)
 	}
 }
