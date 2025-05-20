e16bb710f6ba12480ce6240a51d4b88ee55af49b
shola <michael.o.situ@gmail.com>
2025-05-19 22:57:22 -0700

    feat: create Update method refactor: use const errors. For better reusability (and immutability/comparison)

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,18 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
-var (
-	ErrNotFound = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -28,6 +33,21 @@ func (d Dictionary) Add(word, definition string) error {
 	default:
 		return err
 	}
-	
+
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newDefinition
+	default:
+		return err
+	}
+
 	return nil
 }
