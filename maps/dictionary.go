7b45ecb9504a0a86fbc0aefe69a2d72e70d0a6d9
shola <michael.o.situ@gmail.com>
2025-05-19 23:04:32 -0700

    feat: create Delete method

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -51,3 +51,17 @@ func (d Dictionary) Update(word, newDefinition string) error {
 
 	return nil
 }
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+	return nil
+}
