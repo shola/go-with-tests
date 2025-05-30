fa8b0abe1c9927e98f42146d5c5f19f9897bc996
shola <michael.o.situ@gmail.com>
2025-05-30 12:51:14 -0700

    refactor: create thin slice of functionality backed by end to end tests

--- mocking/main.go
+++ mocking/main.go
@@ -1,5 +1,15 @@
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-func Countdown(out io.Writer) {}
+func Countdown(out io.Writer) {
+	fmt.Fprintf(out, "3")
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
