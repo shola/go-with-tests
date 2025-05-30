e466af722e0739ff664c0a0e2b2464207e727239
shola <michael.o.situ@gmail.com>
2025-05-30 12:10:02 -0700

    refactor: make Greet work for all params that implement the io.Writer interface

--- di/di.go
+++ di/di.go
@@ -1,13 +1,17 @@
-package di
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-// passing a buffer to Fprintf allows capturing the
-// result of outputting to stdout, making this func
-// testable
-func Greet(writer *bytes.Buffer, name string) {
+func main() {
+	Greet(os.Stdout, "Elodie")
+}
+
+// passing an io.Writer interface param (includes buffer and stdout) to Fprintf allows 
+// capturing the result of outputting to stdout, making this func testable
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
