2764649fff2dbc55e5fa389274bac5328904dd4b
shola <michael.o.situ@gmail.com>
2025-05-30 13:24:00 -0700

    refactor: add a sleeper interface and spySleeper struct to test

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -5,10 +5,20 @@ import (
 	"testing"
 )
 
+// Spy to keep track of args used and how many times the spy is called
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -18,5 +28,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to seleper, want 3 got %d", spySleeper.Calls)
 	}
 }
--- mocking/main.go
+++ mocking/main.go
@@ -11,6 +11,11 @@ const (
 	finalWord = "Go!"
 	countdownStart = 3
 )
+
+type Sleeper interface {
+	Sleep()
+}
+
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
