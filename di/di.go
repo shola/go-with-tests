51b6f42f5d1e5b4f1d8d8127d2a880f6df48e113
shola <michael.o.situ@gmail.com>
2025-05-30 12:19:44 -0700

    refactor: create server endpoint that writes text

--- di/di.go
+++ di/di.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
 )
 
 func main() {
-	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
 }
 
 // passing an io.Writer interface param (includes buffer and stdout) to Fprintf allows 
 // capturing the result of outputting to stdout, making this func testable
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
