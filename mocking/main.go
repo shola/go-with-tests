7f77a71563023a5eac1ac4fa5c9764ad83aa5f27
shola <michael.o.situ@gmail.com>
2025-05-30 13:30:05 -0700

    refactor: use/create a DefaultSleep struct that implements the Sleeper interface

--- mocking/main.go
+++ mocking/main.go
@@ -16,14 +16,21 @@ type Sleeper interface {
 	Sleep()
 }
 
-func Countdown(out io.Writer) {
+type DefaultSleeper struct {}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprintf(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
