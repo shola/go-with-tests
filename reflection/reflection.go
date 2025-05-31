a4d22978087d9630a3ddb5884c14ef5e0431cc79
shola <michael.o.situ@gmail.com>
2025-05-30 19:38:39 -0700

    test: test map params separately because order can't be specified

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -4,17 +4,44 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
-	
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
 
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
 	}
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+
+	// numberOfValues := 0
+	// var getField func(int) reflect.Value
+
+	// switch val.Kind() {
+	// case reflect.Struct:
+	// 	numberOfValues = val.NumField()
+	// 	getField = val.Field
+	// case reflect.Map:
+	// 	for _, key := range val.MapKeys() {
+	// 		walk(val.MapIndex(key).Interface(), fn)
+	// 	}
+	// case reflect.Slice, reflect.Array:
+	// 	numberOfValues = val.Len()
+	// 	getField = val.Index
+	// case reflect.String:
+	// 	fn(val.String())
+	// }
+
+	// for i := 0; i < numberOfValues; i++ {
+	// 	walk(getField(i).Interface(), fn)
+	// }
 }
 
 func getValue(x interface{}) reflect.Value {
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -61,6 +61,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"slices",
+			[]Profile {
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"arrays",
+			[2]Profile {
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},		
 	}
 	
 	for _, test := range cases {
@@ -75,5 +91,29 @@ func TestWalk(t *testing.T) {
 			}
 		})
 	}
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Cow": "Moo",
+			"Sheep": "Baa",
+		}
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+	})
+}
 
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %v to contain %q but it didn't", haystack, needle)
+	}
 }
