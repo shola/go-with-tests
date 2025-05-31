8dc8ea9eb1f18721abbc9c4d03af5c9f9025f531
shola <michael.o.situ@gmail.com>
2025-05-30 18:51:07 -0700

    feat: create simple walk method that calls fn on each item in an interface test: add table based tests

---
+++ reflection/reflection_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct{
+		name string
+		input interface{}
+		expectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				name string
+				city string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+	}
+	
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			var got []string
+			walk(test.input, func(input string) {
+				got = append(got, input)
+			})
+		
+			if !reflect.DeepEqual(got, test.expectedCalls) {
+				t.Errorf("got %q, want %q", got, test.expectedCalls)
+			}
+		})
+	}
+
+}
