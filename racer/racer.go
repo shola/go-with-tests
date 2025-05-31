8c2a8e70341e30886da0af962cb5268b48043eed
shola <michael.o.situ@gmail.com>
2025-05-30 17:31:00 -0700

    refactor: make a configurable racer so timeout can be adjusted to more easily test the unhappy path

--- racer/racer.go
+++ racer/racer.go
@@ -6,7 +6,13 @@ import (
 	"time"
 )
 
+const tenSecondTimeout = 10 * time.Second
+
 func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 	// `select` allows you to wait on multiple channels, and the
 	// first one wins
 	select {
@@ -14,7 +20,7 @@ func Racer(a, b string) (winner string, err error) {
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
--- racer/racer_test.go
+++ racer/racer_test.go
@@ -29,23 +29,22 @@ func TestRacer(t *testing.T) {
 		want := fastURL
 		got, err := Racer(slowURL, fastURL)
 	
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-
 		if err != nil {
 			t.Errorf("got error %q, expected nil", err)
 		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	})
 
-	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+	t.Run("returns an error if a server doesn't respond before timeout", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+		timeout := 20 * time.Millisecond
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, timeout)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
