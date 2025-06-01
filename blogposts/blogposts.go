e579d183f63c324316af4e4b25151de6fefe6178
shola <michael.o.situ@gmail.com>
2025-06-01 13:58:39 -0700

    refactor: create helper for creating a new post

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -32,6 +32,10 @@ func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
 		return Post{}, err
 	}
 
+	return newPost(postFile)
+}
+
+func newPost(postFile fs.File) (Post, error) {
 	postData, err := io.ReadAll(postFile)
 	if err != nil {
 		return Post{}, err
