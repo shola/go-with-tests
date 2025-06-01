510c640ead0d3e45061cd604ad7cd39adb852d69
shola <michael.o.situ@gmail.com>
2025-06-01 14:04:28 -0700

    refactor: extract post assertion helper

--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -37,6 +37,11 @@ func TestNewBlogPosts(t *testing.T) {
 	got := posts[0]
 	want := blogposts.Post{Title: "Post 1"}
 
+	assertPost(t, got, want)
+}
+
+func assertPost(t testing.TB, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %+v, want %+v", got, want)
 	}
