31ade6d8ee42ba4f8908df303ae0e5bc9c10f3b4
shola <michael.o.situ@gmail.com>
2025-06-01 15:25:55 -0700

    refactor: write enough code to make test pass refactor: scan lines while they exist, and write them to a buffer, then return that buffer as a string

--- blogposts/post.go
+++ blogposts/post.go
@@ -2,6 +2,8 @@ package blogposts
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -28,11 +30,25 @@ func newPost(postFile io.Reader) (Post, error) {
 		return val
 	}
 
+	readBodyContent := func() string {
+		// throw away the "---" line... I don't love how brittle this is when the
+		// functions aren't called in a precise order
+		scanner.Scan()
+
+		buf := bytes.Buffer{}
+		for scanner.Scan() {
+			fmt.Fprintln(&buf, scanner.Text())
+		}
+
+		return strings.TrimSuffix(buf.String(), "\n")
+	}
+
 	// TODO: add error handling
 
 	return Post{
 		Title:       readMetaLine(titleSeparator),
 		Description: readMetaLine(descriptionSeparator),
 		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBodyContent(),
 	}, nil
 }
