ee519b06ee836c7fab79dc9b927a1be816414eec
shola <michael.o.situ@gmail.com>
2025-06-01 15:41:44 -0700

    refactor: break out scanning logic into helpers

--- blogposts/post.go
+++ blogposts/post.go
@@ -24,31 +24,29 @@ type Post struct {
 func newPost(postFile io.Reader) (Post, error) {
 	scanner := bufio.NewScanner(postFile)
 
-	readMetaLine := func(tagName string) string {
-		scanner.Scan()
-		val := strings.TrimPrefix(scanner.Text(), tagName)
-		return val
-	}
-
-	readBodyContent := func() string {
-		// throw away the "---" line... I don't love how brittle this is when the
-		// functions aren't called in a precise order
-		scanner.Scan()
-
-		buf := bytes.Buffer{}
-		for scanner.Scan() {
-			fmt.Fprintln(&buf, scanner.Text())
-		}
-
-		return strings.TrimSuffix(buf.String(), "\n")
-	}
-
 	// TODO: add error handling
 
 	return Post{
-		Title:       readMetaLine(titleSeparator),
-		Description: readMetaLine(descriptionSeparator),
-		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
-		Body:        readBodyContent(),
+		Title:       readMetaLine(scanner, titleSeparator),
+		Description: readMetaLine(scanner, descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(scanner, tagsSeparator), ", "),
+		Body:        readBodyContent(scanner),
 	}, nil
+}
+
+func readMetaLine(scanner *bufio.Scanner, tagName string) string {
+	scanner.Scan()
+	return strings.TrimPrefix(scanner.Text(), tagName)
+}
+func readBodyContent(scanner *bufio.Scanner) string {
+	// throw away the "---" line... I don't love how brittle this is when the
+	// functions aren't called in a precise order
+	scanner.Scan()
+
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+
+	return strings.TrimSuffix(buf.String(), "\n")
 }
