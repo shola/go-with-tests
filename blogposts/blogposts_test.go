9ef3640f1d54dcd80a92b7acee01e3dea0ae226d
shola <michael.o.situ@gmail.com>
2025-06-01 13:44:31 -0700

    refactor: test for errors

--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -3,11 +3,20 @@ package blogposts_test
 // best practice to put tests in their own package
 
 import (
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 
 	blogposts "github.com/shola/blogposts"
 )
+
+type StubFailingFS struct {
+}
+
+func (s *StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("on no, I always fail")
+}
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
@@ -15,8 +24,11 @@ func TestNewBlogPosts(t *testing.T) {
 		"hello-world2.md": {Data: []byte("hola")},
 	}
 
-	posts := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromFS(fs)
 
+	if err != nil {
+		t.Fatal(err)
+	}
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
