ea1cc39ab2eca0e84c5dd5d04971e86c287eaa95
shola <michael.o.situ@gmail.com>
2025-06-01 15:00:06 -0700

    fix: include the whitespace when splitting

--- blogposts/post.go
+++ blogposts/post.go
@@ -32,6 +32,6 @@ func newPost(postFile io.Reader) (Post, error) {
 	return Post{
 		Title:       readMetaLine(titleSeparator),
 		Description: readMetaLine(descriptionSeparator),
-		Tags:        strings.Split(readMetaLine(tagsSeparator), ","),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
 	}, nil
 }
