41b1982f47009d97bc44a526b1f8e0bd45db72c3
shola <michael.o.situ@gmail.com>
2025-06-01 21:27:58 -0700

    refactor: calculate balance

--- collections_with_generics/transaction.go
+++ collections_with_generics/transaction.go
@@ -3,9 +3,22 @@ package collections
 type Transaction struct {
 	From string
 	To   string
-	Sum  int
+	Sum  float64
 }
 
 func BalanceFor(transactions []Transaction, name string) float64 {
-	return 0.0
+	// createLedger := func(accum float64, item Transaction) Transaction {
+	// 	// do addition and subtraction for each in the pair
+	// }
+	// return Reduce(transactions, createLedger, 0.0)
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
+		}
+	}
+	return balance
 }
